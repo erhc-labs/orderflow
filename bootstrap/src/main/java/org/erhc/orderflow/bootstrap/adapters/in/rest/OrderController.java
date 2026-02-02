d254682f8e00556f91626a0f0413ebf1acfa3059
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-02-02 10:53:44 -0500

    feat(bootstrap, core): Añadir funcionalidad de listado de órdenes - Implementar ListOrderUseCase e integrarlo en el controlador REST. - Añadir DTO OrderResponse y OrderRestMapper para el mapeo de respuestas. - Expandir el modelo Order y OrderRespositoryPort con capacidades de listado. - Añadir mapeo de persistencia en MongoDB para órdenes e ítems.

--- bootstrap/src/main/java/org/erhc/orderflow/bootstrap/adapters/in/rest/OrderController.java
+++ bootstrap/src/main/java/org/erhc/orderflow/bootstrap/adapters/in/rest/OrderController.java
@@ -1,22 +1,27 @@
 package org.erhc.orderflow.bootstrap.adapters.in.rest;
 
+import org.erhc.orderflow.bootstrap.adapters.in.rest.dto.OrderResponse;
+import org.erhc.orderflow.bootstrap.adapters.in.rest.mapper.OrderRestMapper;
 import org.erhc.orderflow.core.application.request.CreateOrderRequest;
+import org.erhc.orderflow.core.domain.model.Order;
 import org.erhc.orderflow.core.domain.valueobject.OrderId;
 import org.erhc.orderflow.core.ports.in.CreateOrderUseCase;
+import org.erhc.orderflow.core.ports.in.ListOrdersUseCase;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
     private final CreateOrderUseCase createOrderUseCase;
+    private final ListOrdersUseCase listOrderUseCase;
 
-    public OrderController(CreateOrderUseCase createOrderUseCase) {
+    public OrderController(CreateOrderUseCase createOrderUseCase, ListOrdersUseCase listOrderUseCase) {
         this.createOrderUseCase = createOrderUseCase;
+        this.listOrderUseCase = listOrderUseCase;
     }
 
     @PostMapping
@@ -25,6 +30,15 @@ public class OrderController {
     ){
         OrderId id = createOrderUseCase.create(request);
         return ResponseEntity.ok(id.value());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> list(){
+        return ResponseEntity.ok(
+                listOrderUseCase.list().stream()
+                        .map(OrderRestMapper::toResponse)
+                        .toList()
+        );
     }
 
 }
